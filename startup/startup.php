75e3b8487cff1d770a7c0d9592f68fa913a1129e
Takagero <OpeLLIek_o0@mail.ru>
2016-07-12 21:10:31 +0300

    Создали файл startup с функцией автозагрузки классов

---
+++ startup/startup.php
@@ -0,0 +1,23 @@
+<?php
+
+    // Загрузка классов «на лету»
+function __autoload($class_name) {
+
+            $filename = strtolower($class_name) . '.php';
+
+            $file = site_path . 'classes' . DIRSEP . $filename;
+
+            if (file_exists($file) == false) {
+
+                    return false;
+
+            }
+
+
+            include ($file);
+
+    }
+
+$registry = new Registry;
+
+?>
