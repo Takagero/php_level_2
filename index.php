01e429f266ee920872964a4f3fb54c9b32fa1e6a
Takagero <OpeLLIek_o0@mail.ru>
2016-07-12 21:08:08 +0300

    Добавили инклуд фала startup Подключили бд через PDO

--- index.php
+++ index.php
@@ -10,4 +10,14 @@ define ('DIRSEP', DIRECTORY_SEPARATOR);
 $site_path = realpath(dirname(__FILE__) . DIRSEP . '.' . DIRSEP) . DIRSEP ;
 define ('site_path', $site_path);
 
+// Startup
+require_once($site_path . 'startup' . DIRSEP . 'startup.php');
+
+# Соединяемся с БД
+$db = new PDO('mysql:host=localhost;dbname=super_blog', 'root', ''); //excellent
+
+$registry->set ('db', $db);
+
+//В этом примере мы сначала создаём новый экземпляр библиотеки PDO и соединяемся с нашей БД MySQL. Потом делаем переменную $db доступной глобально при помощи нашего класса Registry.
+
 ?>
